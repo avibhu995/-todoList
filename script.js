312631a2ee8ad9eba099953f9c9d0ff6e9cfa770
Awanish Yadav <120742080+avibhu995@users.noreply.github.com>
2024-03-20 18:15:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById ('form').addEventListener('Submit',function(event){ event.preventDefault();});
+document.getElementById ('signin form').addEventListener('Submit',function(event){ event.preventDefault();});
+document.getElementById ('forgot form').addEventListener('Submit',function(event){ event.preventDefault();});
+
+document.getElementById ('signin link').addEventListener('click',function(event){ event.preventDefault();
+document.getElementById('Signup').style.display = 'none';
+ document.getElementById('signin').style.display = 'none';
+ document.getElementById('forgot').style.display = 'none';
+});
+document.getElementById ('forgotlink').addEventListener('click',function(event){ event.preventDefault();
+    document.getElementById('Signup').style.display = 'none';
+     document.getElementById('signin').style.display = 'none';
+     document.getElementById('forgot').style.display = 'block';
+    });
+    document.getElementById ('todo btn').addEventListener('click',function(event){ event.preventDefault();
+    });
+
+
+
+
+
